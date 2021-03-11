8fa25ac4083fb4a4d185fba5ad4127d838327129
Reiko0415 <62266621+Reiko0415@users.noreply.github.com>
2021-03-11 12:39:53 +0900

    XMLデータの表示

--- HelloController.php
+++ HelloController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class HelloController extends AbstractController
 {
@@ -19,21 +20,19 @@ class HelloController extends AbstractController
 
     public function index(Request $request)
     {
+        $encoders = array(new XmlEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers,$encoders);
+
        $data = array(
            'name'=>array('first'=>'Taro','second'=>'Yamada'),
                     'age'=>36,'mail'=>'taro@yamada.kun'
        );
-       return new JsonResponse($data);
+       $response = new Response();
+       $response->headers->set('Content-Type','xml');
+       $result = $serializer->serialize($data,'xml');
+       $response->setContent($result);
+       return $response;
     }
 
-    /**
-     * @Route("/other/{domain}" , name="other")
-     */
-    public function other(Request $request,$domain=''){
-        if ($domain == ''){
-            return $this->redirect('/hello');
-        }else{
-            return new RedirectResponse("http://{$domain}.com");
-        }
-    }
 }
