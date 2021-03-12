90b7f5cf8ffddfc4531c8808abf35f8d2ab3bcd7
Reiko0415 <62266621+Reiko0415@users.noreply.github.com>
2021-03-13 05:44:59 +0900

    テンプレートからコントローラーへ値を渡す

--- HelloController.php
+++ HelloController.php
@@ -9,16 +9,32 @@ class HelloController extends AbstractController
 {
 
     /**
-     * @Route("/hello" , name="hello")
+     * @Route("/hello/{msg}" , name="hello")
      */
 
-    public function index()
+    public function index($msg='Hello!')
     {
         return $this->render('hello/index.html.twig',[
-            'controller_name' => 'HelloController',
+            'controller' => 'HelloController',
+            'action' => 'index',
+            'prev_action' => '(none)',
+            'message' => $msg,
         ]);
         
     }
       
-    
+     /**
+     * @Route("/other/{action}/{msg}" , name="other")
+     */
+
+    public function other($action,$msg)
+    {
+        return $this->render('hello/index.html.twig',[
+            'controller' => 'HelloController',
+            'action' => 'other',
+            'prev_action' => $action,
+            'message' => $msg,
+        ]);
+        
+    }
 }
