ed7832067f96e12608af45e3f98f42121086d330
Reiko0415 <62266621+Reiko0415@users.noreply.github.com>
2021-03-23 12:44:10 +0900

    レコードの作成

--- HelloController.php
+++ HelloController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class HelloController extends AbstractController
 {
@@ -55,6 +56,35 @@ class HelloController extends AbstractController
       ]);
       
    }
+
+   /**
+    * @Route("/create", name="create")
+    */
+    public function create(Request $request){
+      $person = new Person();
+      $form = $this->createFormBuilder($person)
+         ->add('name',TextType::class)
+         ->add('mail',TextType::class)
+         ->add('age',IntegerType::class)
+         ->add('save',SubmitType::class,array('label'=> 'Click'))
+         ->getForm();
+
+      if($request->getMethod() == 'POST'){
+         $form->handleRequest($request);
+         $person = $form->getData();
+         $manager = $this->getDoctrine()->getManager();
+         $manager->persist($person);
+         $manager->flush();
+         return $this->redirect('/hello');
+      }else{
+         return $this->render('hello/create.html.twig',[
+         'title' => 'Hello',
+         'message' => 'Create Entity',
+         'form' => $form->createView(),
+      ]);}
+      
+   }
+
 }
 
    class FindForm{
