087471dcdff16bd7f362b1b5f6e707f40f9e2a31
Reiko0415 <62266621+Reiko0415@users.noreply.github.com>
2021-04-04 05:35:35 +0900

    ネイティブSQLの実行

--- HelloController.php
+++ HelloController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 class HelloController extends AbstractController
 {
@@ -46,15 +47,21 @@ class HelloController extends AbstractController
          ->getRepository(Person::class);
 
       $manager = $this->getDoctrine()->getManager();
+      $mapping = new ResultSetMappingBuilder($manager);
+      $mapping->addRootEntityFromClassMetadata('App\Entity\Person','p');
 
       if($request->getMethod() == 'POST'){
          $form->handleRequest($request);
          $findstr = $form->getData()->getFind();
-         $query = $manager->createQuery(
-            "SELECT p FROM App\Entity\Person p WHERE p.name = '{$findstr}'");
+         $arr = explode(',',$findstr);
+         $query = $manager->createNativeQuery(
+            'SELECT * FROM person WHERE age between ?1 AND ?2',$mapping)
+            ->setParameters(array(1 => $arr[0],2 => $arr[1]));
          $result = $query->getResult();
       }else{
-         $result = $repository->findAllwidthSort();
+         $query = $manager->createNativeQuery(
+            'SELECT * FROM person',$mapping);
+         $result = $query->getResult();
       }
       return $this->render('hello/find.html.twig',[
          'title' => 'Hello',
