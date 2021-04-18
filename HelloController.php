ff6796e282cf2a5d352ec50b6f4a653ab4d6b31b
Reiko0415 <62266621+Reiko0415@users.noreply.github.com>
2021-04-19 07:27:11 +0900

    nameによる名前の設定

--- HelloController.php
+++ HelloController.php
@@ -29,7 +29,7 @@ class HelloController extends AbstractController
    {
       $finder = new Finder();
 
-      $finder->files()->depth('<=1')->in('../templates/');
+      $finder->files()->depth('<=2')->name('*.yaml')->in('../../');
 
       return $this->render('hello/index.html.twig', [
            'title' => 'Hello',
